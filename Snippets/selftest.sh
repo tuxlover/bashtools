e86ad17d5362d857f26348dc6b1d85d26573fcd2
Matthias Propst <penguinuser@web.de>
2009-08-14 00:38:25 +0200

    New Snippet selftest

---
+++ Snippets/selftest.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#selfintegrtety check
+
+SHA_SUM=/var/log/selftest
+FIRST_RUN=
+
+option_f()
+{
+FIRST_RUN="yes"	
+}
+
+
+while getopts f opt
+	do
+		case "$opt" in
+			f) option_f
+			;;
+			\?) echo "not an option"
+		esac
+	done
+shift `expr $OPTIND - 1`
+
+if [ -z $FIRST_RUN ]
+	then
+		FIRST_RUN="no"
+fi
+
+if [ ! -f $SHA_SUM/init/SHA_SUMS ]
+	then
+		echo "first run of $0. initilize sha512 checksums."
+		mkdir -p $SHA_SUM/init
+		{
+			sha512sum $0
+			sha512sum /usr/bin/clamscan
+			sha512sum /usr/bin/freshclam
+			sha512sum /usr/bin/lynis
+			sha512sum /usr/bin/rkhunter
+			sha512sum /bin/rpm			
+		} >> $SHA_SUM/init/SHA_SUMS
+		#imutable the SHA_SUMS because this is the weakest part of the script
+		chattr +i $SHA_SUM/init/SHA_SUMS
+	else
+		if [ "$FIRST_RUN" != "no" ]
+			then
+				chattr -i $SHA_SUM/init/SHA_SUMS
+				cat /dev/null > $SHA_SUM/init/SHA_SUMS
+				{
+					sha512sum $0
+					sha512sum /usr/bin/clamscan
+					sha512sum /usr/bin/freshclam
+					sha512sum /usr/bin/lynis
+					sha512sum /usr/bin/rkhunter
+					sha512sum /bin/rpm			
+				} >> $SHA_SUM/init/SHA_SUMS
+		
+				chattr +i $SHA_SUM/init/SHA_SUMS
+			else
+				sha512sum -c $SHA_SUM/init/SHA_SUMS
+		fi
+fi
+
+exit 0
+
+
+#option -f firstrun pretend to be the firstrun. this is usefull if you update your system and therefore the sha512sums dont match anymore
+#warning: be sure that you do this only when the system has been updated by rpm or whatever package manager you are using
+#check all binaries in clamav, lynsis, rkhunter, rpm or which might be used by them.
