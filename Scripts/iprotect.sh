c37bc8166a406c817d79d4821afb7993c3cb336b
l1zard <l1zard@Marvin.site>
2010-11-02 16:48:04 +0100

    New Script: iprotect. switch icmp redirects off and on

---
+++ Scripts/iprotect.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#disable and enable icmp redirection
+
+STATUS=$1
+
+#test whether we have a non empty variable
+if [ -z $STATUS	]
+	then	
+		echo "Usage: $0 on off status"
+		exit 1
+fi
+
+
+status()
+{
+
+FILES=(/proc/sys/net/ipv4/conf/all/accept_redirects /proc/sys/net/ipv4/conf/all/send_redirects /proc/sys/net/ipv6/conf/all/accept_redirects)
+
+echo "Security mode is"
+
+for f in ${FILES[@]}
+	do
+		if [ $(cat $f) == "1" ]
+			then
+				echo "$f" && echo -e "\E[32m  activated"; tput sgr0
+			else
+				echo "$f" && echo -e "\E[31m deactivated"; tput sgr0
+		fi
+	done	
+
+}
+
+on()
+{
+FILES=(/proc/sys/net/ipv4/conf/all/accept_redirects /proc/sys/net/ipv4/conf/all/send_redirects /proc/sys/net/ipv6/conf/all/accept_redirects)
+
+	
+for f in ${FILES[@]}
+	do
+				echo "1" > $f
+				echo "$f" && echo -e "\E[32m  activated"; tput sgr0
+	done	
+}
+
+off()
+{
+FILES=(/proc/sys/net/ipv4/conf/all/accept_redirects /proc/sys/net/ipv4/conf/all/send_redirects /proc/sys/net/ipv6/conf/all/accept_redirects)
+
+	
+for f in ${FILES[@]}
+	do
+				echo "0" > $f
+				echo "$f" && echo -e "\E[31m  deactivated"; tput sgr0
+	done		
+	
+}
+
+
+#check whether we are root
+if [ $UID -ne 0 ]
+	then
+		echo "not root"
+		exit 1
+fi	
+
+
+case $STATUS in
+	on) on
+	;;
+	off) off
+	;;
+	status) status
+esac
