336b324c3aa2ed67cbaf0c62eccf31b0d86e6f07
l1zard <propstmatthias@googlemail.com>
2011-05-23 01:33:48 +0200

    update_sync.sh: small fixes

--- Scripts/update_sync.sh
+++ Scripts/update_sync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #update_sync.sh
 
-#scrip that is indent to check via rsync wheter there is a change of one of the destinations
-#if there is a difference sync between both and resolve onky newer 
+#scrip that is intend to check via rsync wheter there is a change of one of the destinations
+#if there is a difference sync between both and resolve only newer 
 
 
 if [ ! -d ~/test ]
