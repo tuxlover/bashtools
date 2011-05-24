436c094fe8b0274e189ab878de8d5dee24c5b2a0
l1zard <propstmatthias@googlemail.com>
2011-05-24 16:50:00 +0200

    todo.sh: removed the -c option

--- Scripts/todo.sh
+++ Scripts/todo.sh
@@ -4,32 +4,14 @@
 BAK=$HOME/.todo.lst.bak
 TODO_LIST_FILE=$HOME/.todo.lst
 
-
-create_list()
-{
-if [ ! -f $TODO_LIST_FILE ]
-	then
-		touch $TODO_LIST_FILE
-fi
-}
-
-
 add_entry()
 {
 #TODO: test for "" at beginning and end of the note
 if [ ! -f $TODO_LIST_FILE ]
 	then
 		echo "$TODO_LIST_FILE was not created yet."
-		read -e -t 10 -n 1 -p "Do you want to create this file now y/n" ANSWER
-		${ANSWER:=n} 2> /dev/null
-
-		if [ $ANSWER == "y" ]
-			then
-				touch $TODO_LIST_FILE
-			else
-				echo "Use -c option to create the file for the todo list."
-				exit
-		fi
+		touch $TODO_LIST_FILE
+		echo "created new todo list file in $TODO_LIST_FILE"
 fi
 NEW_ENTRY=$(echo ${OPTARG[@]})
 echo "$NEW_ENTRY --> [o]" >> $TODO_LIST_FILE
@@ -85,24 +67,21 @@ sed -i '/.* --> \[x\]/d' $TODO_LIST_FILE
 help_me()
 {
 echo "$0 is a script for easyly maintianing a todo list"
-echo "-c: create the todo list file if its not exist. the script will not remove existing files"
-echo "-a: add a new entry to the todo list. Example: $0 -a \"write an entry\""
+echo "-a: add a new entry to the todo list. Example: todo -a \"write an entry\""
 echo "-s: show all the entries in the todo list." 
 echo "-o: show all open entries in the todo list [o]"
 echo "-d: show all entries marked as done  [x]"
 echo "-r remove entries from the todo list which are marked as done [x]"
-echo "-u unmark an entry as done. Example: $0 -u 3 will mark the entry number again as undone"
-echo "-x: mark an entry as done. Example: $0 -x 3 will mark the entry number 3 as done."
+echo "-u unmark an entry as done. Example: todo -u 3 will mark the entry number again as undone"
+echo "-x: mark an entry as done. Example: todo -x 3 will mark the entry number 3 as done."
 echo "-h: show this help"
 }
 
 #begin options
-while getopts "cdhorsa:u:x:" opt
+while getopts "chorsa:u:x:" opt
 	do
 		case $opt in 
 			a) add_entry
-				;;
-			c) create_list
 				;;
 			d) show_done
 				;;
@@ -122,5 +101,4 @@ while getopts "cdhorsa:u:x:" opt
 	done
 shift `expr $OPTIND - 1`	
 
-#TODO: the -c option is not neccesary
 #TODO: disable the noclobber option if this was enabled
