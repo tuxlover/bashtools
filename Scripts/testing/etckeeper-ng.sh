6932eca01c58b7d4abae45cbfbd371dc37dcc56f
matthias <propstmatthias@googlemail.com>
2011-10-12 16:05:57 +0200

    /testing/etckeeper-ng.sh: corrected english grammar

--- Scripts/testing/etckeeper-ng.sh
+++ Scripts/testing/etckeeper-ng.sh
@@ -17,7 +17,7 @@ EXCLUDEFILE="/root/.etcbackup/excludes"
 get_help()
 {
 echo "$0 [ -i ] [ -b ] [ -l ] [ -r Branch ] [ -h ]"
-echo "etckeeper-ng can do a snapshot based backup of the /etc folder using git version control"
+echo "etckeeper-ng helps you creating a snapshot based backup of the /etc folder using git version control"
 echo "-i: do the initial backup. there must be an initial backup to do new branched backups"
 echo "-b: do a new branch backup. if no initiallized backup exists you will be asked"
 echo "-c: check the /etc direcotry for changes"
