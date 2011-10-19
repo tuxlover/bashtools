32c2e92b772c8050d3501113d8e8bd131a301c01
matthias <propstmatthias@googlemail.com>
2011-10-19 13:39:41 +0200

    new file: mkalbum.sh; testing/etckeeper-ng: small fixes. new ideas in comments

--- Scripts/testing/etckeeper-ng.sh
+++ Scripts/testing/etckeeper-ng.sh
@@ -325,33 +325,33 @@ if [ "$OPTARG" == "HEAD" ]
 					fi
 					lof=$((lof-=1))
 				done
-else
-		cd $BACKUPDIR
-		git checkout master
-		git reset --hard "$OPTARG" || echo "This commit does not exist. Use -l to show commits."
-		git_status_file="/tmp/git_status_file"
-		git status -s > $git_status_file
-		of=$(wc -l $git_status_file|awk '{print $1}')
-		until  [ "$lof" == 0  ]
-			do	
-				if [ $(tail -n $lof $git_status_file|head -1|awk '{print $1}') == "M" 2> /dev/null ]
-					then
-						:
-				elif [ $(tail -n $lof $git_status_file|head -1|awk '{print $1}') == "D"  2> /dev/null ]
-					then
-						:
-				elif [ $(tail -n $lof $git_status_file|head -1|awk '{print $1}') == "A" 2> /dev/null ]
-					then
-						:
-				elif [$(tail -n $lof $git_status_file|head -1|awk '{print $1}') == "R" 2> /dev/null ]
-					then
-						:
-				else
-					new_file=$(tail -n $lof $git_status_file|head -1|awk '{print $2}')
-					rm $new_file
-				fi
-				lof=$((lof-=1))
-			done
+		else
+			cd $BACKUPDIR
+			git checkout master
+			git reset --hard "$OPTARG" || echo "This commit does not exist. Use -l to show commits."
+			git_status_file="/tmp/git_status_file"
+			git status -s > $git_status_file
+			of=$(wc -l $git_status_file|awk '{print $1}')
+			until  [ "$lof" == 0  ]
+				do	
+					if [ $(tail -n $lof $git_status_file|head -1|awk '{print $1}') == "M" 2> /dev/null ]
+						then
+							:
+					elif [ $(tail -n $lof $git_status_file|head -1|awk '{print $1}') == "D"  2> /dev/null ]
+						then
+							:
+					elif [ $(tail -n $lof $git_status_file|head -1|awk '{print $1}') == "A" 2> /dev/null ]
+						then
+							:
+					elif [$(tail -n $lof $git_status_file|head -1|awk '{print $1}') == "R" 2> /dev/null ]
+						then
+							:
+					else
+						new_file=$(tail -n $lof $git_status_file|head -1|awk '{print $2}')
+						rm $new_file
+					fi
+					lof=$((lof-=1))
+				done
 fi
 rsync -rtpog -clis $BACKUPDIR/etc/ /etc/
 COMMENT=(backup after restore)
@@ -578,3 +578,4 @@ exit 0
 #Todo:
 #use return values and do checks before reading options
 #report of changed files in diff report file when found in function compare
+#use a config file for configuring how etckeeper behaves
